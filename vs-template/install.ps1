a277fbf55cf5f4f09dc9b0747687d05f36d49ecc
Pascal Senjic <pascal.senjic@gmail.com>
2016-03-16 19:19:13 +0100

    Installation and uninstallation powershell scripts

---
+++ vs-template/install.ps1
@@ -0,0 +1,28 @@
+﻿Write-Host "Deploy project template for Visual Studio 2015"
+Write-Host "Deploying project template into custom templates"
+$installationPath = "$env:USERPROFILE";
+$installationPath = "$installationPath\documents\Visual Studio 2015\Templates\ProjectTemplates";
+
+
+if(Test-Path ("$installationPath\Visual C#")){
+    Write-Host "Deploy to C#"
+
+    if(Test-Path ("$installationPath\Visual C#\AngularJS.App.zip")){
+        Remove-Item "$installationPath\Visual C#\AngularJS.App.zip"
+    }
+    Copy-Item ".\AngularJS.App.zip" "$installationPath\Visual C#"
+    Write-Host "Template deployed";
+
+}
+
+if(Test-Path ("$installationPath\Visual Web Developer")){
+
+Write-Host "Deploy to Web Developer"
+if(Test-Path ("$installationPath\Visual Web Developer\AngularJS.App.zip")){
+        Remove-Item "$installationPath\Visual Web Developer\AngularJS.App.zip"
+    }
+    Copy-Item ".\AngularJS.App.zip" "$installationPath\Visual Web Developer"
+    Write-Host "Template deployed";
+}
+
+Read-Host "Deployment successfully finished. Press any key to continue...";
---
+++ vs-template/uninstall.ps1
@@ -0,0 +1,26 @@
+﻿Write-Host "Removing project template for Visual Studio 2015"
+Write-Host "Remove project template from custom templates"
+$installationPath = "$env:USERPROFILE";
+$installationPath = "$installationPath\documents\Visual Studio 2015\Templates\ProjectTemplates";
+
+
+if(Test-Path ("$installationPath\Visual C#")){
+    Write-Host "Remove to C#"
+
+    if(Test-Path ("$installationPath\Visual C#\AngularJS.App.zip")){
+        Remove-Item "$installationPath\Visual C#\AngularJS.App.zip"
+    }
+    Write-Host "Template removed";
+
+}
+
+if(Test-Path ("$installationPath\Visual Web Developer")){
+
+Write-Host "Remove to Web Developer"
+if(Test-Path ("$installationPath\Visual Web Developer\AngularJS.App.zip")){
+        Remove-Item "$installationPath\Visual Web Developer\AngularJS.App.zip"
+    }
+    Write-Host "Template removed";
+}
+
+Read-Host "Removing successfully finished. Press any key to continue...";
